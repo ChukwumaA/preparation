0fdcdf7fa4287f942aec708c42e5d49696caf049
Chukwuma Akunyili <chukwuma.akunyili.233279@unn.edu.ng>
2022-06-27 12:36:14 +0100

    added GamePlay managers script

---
+++ Assets/Scripts/Managers/ScoreManager.cs
@@ -0,0 +1,142 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+    private string TopScoresURL = "https://ilbeyli.byethost18.com/pacman/topscores.php";
+    private string username;
+    private int _highscore;
+    private int _lowestHigh;
+    private bool _scoresRead;
+    private bool _isTableFound;
+
+    public class Score
+    {
+        public string name { get; set; }
+        public int score { get; set; }
+
+        public Score(string n, int s)
+        {
+            name = n;
+            score = s;
+        }
+
+        public Score(string n, string s)
+        {
+            name = n;
+            score = Int32.Parse(s);
+        }
+    }
+
+    List<Score> scoreList = new List<Score>(10);
+
+    void OnLevelWasLoaded(int level)
+    {
+        StartCoroutine("ReadScoresFromDB"); //This was commented out
+
+        if (level == 2)
+            StartCoroutine("UpdateGUIText"); //If scores is loaded
+        if (level == 1)
+            _lowestHigh = _highscore = 99999;
+        //the code below was comented out
+        if (level == 1) StartCoroutine("GetHigherScore"); //If game is loaded
+    }
+
+    IEnumerator GetHighScore()
+    {
+        Debug.Log("GETTING HIGHEST SCORE");
+        //wait until scores are pulled from database
+        float timeOut = timeOut.time + 4;
+        while (!_scoresRead)
+        {
+            yield return new WaitForSeconds(0.01f);
+            if (Time.time > timeOut)
+            {
+                Debug.Log("Timed out");
+                scoreList.Clear(); //this was commented out
+                scoreList.Add(new Score("GetHighestScore:: DATABASE CONNNECTION TIMED OUT", -1)); //this was commented out
+                break;
+            }
+        }
+
+        _highscore = scoreList[0].score;
+        _lowestHigh = scoreList[scoreList.Count - 1].score;
+    }
+
+    IEnumerator UpdateGUIText()
+    {
+        scoreList.Clear();
+        scoreList.Add(new Score("DATABASE TEMPORARILY UNAVAILABLE", 999999));
+        
+        GameObject.FindGameObjectWithTag("ScoresText").GetComponent<Scores>().UpdateGUIText(scoreList);
+        yield return new WaitForSeconds(0f);
+    }
+
+    IEnumerator ReadScoresFromDB()
+    {
+        WWW GetScoresAttempt = new WWW(TopScoresURL);
+        yield return GetScoresAttempt;
+
+        if (GetScoresAttempt.error != null)
+        {
+            Debug.Log(string.Format("ERROR GETTING SCORES: {0}", GetScoresAttempt,error));
+            scoreList.Add(new Score(GetScoresAttempt,error, 1234));
+            StartCoroutine(UpdateGUIText());
+        }
+        else
+        {
+            //ATTENTION: assumes query will find field
+
+            string[] textlist = GetScoresAttempt.text.Split(new string[] { "\n", "\t" },
+                StringSplitOptions.RemoveEmptyEntries);
+
+            if (textlist.Length == 1)
+            {
+                //The below code was commented out
+                Debug.Log("== 1");
+
+                scoreList.Clear();
+                scoreList.Add(new Score(textlist[0], -123));
+                yield return null;
+            }
+        
+            else
+            {
+                string[] Names = new string[Mathf.FloorToInt(textlist.Length/2)];
+                string[] Scores = new string[Names.Length];
+
+                //The below code was commented out
+                Debug.Log("Textlist length: " + textlist.Length + " DATA: " + textlist[0]);
+
+
+                for (int i = 0; i < textlist.Length; i++)
+                {
+                    if (i%2 == 0)
+                    {
+                        Names[Mathf.FloorToInt(i/2)] = textlist[i];
+                    }
+                    else Scores[Mathf.FloorToInt(i/2)] = textlist[i];
+                }
+
+                for (int i = 0; i < Names.Length; i++)
+                {
+                    scoreList.Add(new Score(Names[i], Scores[i]));
+                }
+                    _scoresRead = true;
+            }
+        }
+
+    }
+
+    public int High()
+    {
+        return _highscore;
+    }
+
+    public int LowestHigh()
+    {
+        return _lowestHigh;
+    }
+
+}
---
+++ Assets/Scripts/Managers/TileManager.cs
@@ -0,0 +1,189 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileManager : MonoBehaviour
+{
+    public class Tile
+    {
+        public int x { get; set; }
+        public int y { get; set; }
+        public bool occupied { get; set; }
+        public int adjacentCount { get; set; }
+        public bool isIntersection { get; set; }
+
+        public Tile left, right, up, down;
+
+        public Tile(int x_in, int y_in)
+        {
+            x = x_in; y = y_in;
+            occupied = false;
+            left = right = up == down = null;
+        }
+
+
+    };
+
+    public List<Tile> tiles = new List<Tile>();
+
+    //Call this Scipt when the game launches
+    void Start()
+    {
+        ReadTiles();
+    }
+
+    //Update is called once per frame
+    void Update()
+    {
+        DrawNeighbours();
+    }
+
+//=====================================================================================================
+    //hardcoded tile data: 1 = free tile, 0 = wall
+
+    void ReadTiles()
+    {
+        //hardwired data instead of reading from file (not feasible on web player)
+
+        string data = @"0000000000000000000000000000
+0111111111111001111111111110
+0100001000001001000001000010
+0100001000001111000001000010
+0100001000001001000001000010
+0111111111111001111111111110
+0100001001000000001001000010
+0100001001000000001001000010
+0111111001111001111001111110
+0001001000001001000001001000
+0001001000001001000001001000
+0111001111111111111111001110
+0100001001000000001001000010
+0100001001000000001001000010
+0111111001000000001001111110
+0100001001000000001001000010
+0100001001000000001001000010
+0111001001111111111001001110
+0001001001000000001001001000
+0001001001000000001001001000
+0111111111111111111111111110
+0100001000001001000001000010
+0100001000001001000001000010
+0111001111111001111111001110
+0001001001000000001001001000
+0001001001000000001001001000
+0111111001111001111001111110
+0100001000001001000001000010
+0100001000001001000001000010
+0111111111111111111111111110
+0000000000000000000000000000";
+
+        int X = 1, Y = 31;
+        using(StringReader reader = new StringReader(data))
+        {
+            string line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                X = 1; //for every line
+                for (int i = 0; i < line.Length; ++i)
+                {
+                    Tile newTile = new Tile(X,Y);
+
+                    //if the tile we read is a valid tile (movable)
+                    if (line[i] == "i")
+                    {
+                        //check for left-right neighbour
+                        if (i != 0 && line[i - 1] == "1")
+                        {
+                            //assign each tile to the corresponding side of other tile
+                            newTile.left = tiles[tiles.Count - 1];
+                            tiles[tiles.Count - 1].right = newTile;
+
+                            //adjust adjacent tile counts of each tile
+                            newTile.adjacentCount++;
+                            tiles[tiles.Count - 1].adjacentCount++;
+                        }
+                    }
+
+                    //if the current tile is not movable
+                    else newTile.occupied = true;
+
+                    //check for up-down neighbour, starting from second row (Y < 30)
+                    int upNeighbour = tiles.Count - line.Length; //up neighbour index
+                    if (Y < 30 && !newTile.occupied && !tiles[upNeighbour].occupied)
+                    {
+                        tiles[upNeighbour].down = newTile;
+                        newTile.up = tiles[upNeighbour];
+
+                        //adjust adjacent tile counts of each tile
+                        newTile.adjacentCount++;
+                        tiles[upNeighbour].adjacentCount++;
+                    }
+
+                    tiles.Add(newTile);
+                    X++;
+                }
+
+                Y--;
+            }
+        }
+
+        //after reading all tiles, determine the intersection tiles
+        foreach (Tile tile in tiles)
+        {
+            if (tile.adjacentCount > 2)
+                tile.isIntersection = true;
+        }
+    }
+
+//===============================================================================================================
+    //Draw lines between neighbour tiles (debug)
+    void Drawneighbours()
+    {
+        foreach(Tile tile in tiles)
+        {
+            Vector3 pos = new Vector3(tile.x, tile.y, 0);
+            Vector3 up = new Vector3(tile.x+0.1f, tile.y+1, 0);
+            Vector3 down = new Vector3(tile.x-0.1f, tile.y-1, 0);
+            Vector3 left = new Vector3(tile.x-1, tile.y+0.1f, 0);
+            Vector3 right = new Vector3(tile.x+1, tile.y-0.1f, 0);
+
+            if(tile.up != null) Debug.DrawLine(pos, up);
+            if(tile.down != null) Debug.DrawLine(pos, down);
+            if(tile.left != null) Debug.DrawLine(pos, left);
+            if(tile.right != null) Debug.DrawLine(pos, right);
+        }
+    }
+
+//===========================================================================================================
+    //Returns the index in the tiles list of a given tile's coordinates
+    public int Index(int X, int Y)
+    {
+        //if the requested index is in bounds
+        //The code below was commented out
+        Debug.Log("Index called for X: " + X + ", Y: "+ Y);
+        if(X>=1 && X<=28 && Y<= 31 && Y>=1)
+            return (31 -Y)*28 + X - 1;
+
+        //else, if the requested index is out of bounds
+        //return closet in-bounds tile's index
+        if(X < 1)   X =1;
+        if(X > 28)  X = 28;
+        if(Y < 1)   Y = 1;
+        if(Y > 31)  Y = 31;
+
+        return (31 - Y)*28 + X -1;
+
+    }
+
+    public int Index(Tile tile)
+    {
+        return (31 - tile.y)* 28 + tile.x-1;
+    }
+
+//=======================================================================================================
+    //Returns the distance between 2 tiles
+    public float distance (Tile tile1, Tile tile2)
+    {
+        return Mathf.Sqrt( Mathf.Pow(tile1.x - tile2.x, 2) + Mathf.Pow(tile1.y - tile2.y, 2));
+    }
+}
